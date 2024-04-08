4ba0d16ec0f95b05251096e8220337b5c372a491
Ayush Mahatara <aym0168@my.londonmet.ac.uk>
2024-04-08 13:19:56 +0545

    update

--- .history/app/Filament/Resources/StudentResource_20240408093519.php
+++ .history/app/Filament/Resources/StudentResource_20240408093519.php
@@ -26,7 +26,7 @@ class StudentResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
-    protected static ?string $recordTitleAttribute = 'name';
+
 
     public static function form(Form $form): Form
     {
